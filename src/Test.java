53eeb112da5fa9aca2a7e16c4e472828362929a5
deesh1211 <deesh1211@users.noreply.github.com>
2015-03-28 13:45:47 +0530

    Test class added Please use this class for testing

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (C) 2015 Team FantasticFive
+ * 
+ * This file is part of Project RealEstate.
+ * 
+ * Project RealEstate can not be copied and/or distributed without the express
+ * permission of Team FantasticFive
+ *******************************************************************************/
+
+/**
+ *
+ * @author Kavindu
+ */
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Please use this class for testing");
+
+	}
+
+}

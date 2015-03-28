b72e5d73e40352998258d82d33ce2789126ca7e1
deesh1211 <deesh1211@users.noreply.github.com>
2015-03-28 13:04:56 +0530

    Copyright statement added

--- src/RealEstate.java
+++ src/RealEstate.java
@@ -1,10 +1,21 @@
+/*******************************************************************************
+ * Copyright (C) 2015 Team FantasticFive
+ * 
+ * This file is part of Project RealEstate.
+ * 
+ * Project RealEstate can not be copied and/or distributed without the express
+ * permission of Team FantasticFive
+ *******************************************************************************/
+
+/**
+ *
+ * @author Tharkana
+ */
 
 public class RealEstate {
 
-	
 	public static void main(String[] args) {
 		System.out.println("blaaaaaaaa");
 	}
-	
-	
+
 }

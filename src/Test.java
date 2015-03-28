842e4fa19eda38dcd106d0e822ba2bd6ae3a69c5
deesh1211 <deesh1211@users.noreply.github.com>
2015-03-29 00:32:41 +0530

    new sout added (demo commit) (please tick only ur class dont tick on .sthing files)

--- src/Test.java
+++ src/Test.java
@@ -16,6 +16,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Please use this class for testing");
+		System.out.println("Committing example"
+				+ "Before make any changes always pull the project from the GIT"
+				+ "This is how ur gonna commit the code"
+				+ "Save ur changes first"
+				+ "");
 
 	}
 

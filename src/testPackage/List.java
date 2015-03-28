9cfadb1344172d659ce1bfdb8e32517ae4b31300
Poorni Yasodara <poorni.yasodara@gmail.com>
2015-03-29 01:29:11 +0530

    Committing SortedList Class and List Class

---
+++ src/testPackage/List.java
@@ -0,0 +1,16 @@
+package testPackage;
+
+public class List {
+
+	public int compareTo(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int copy() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
